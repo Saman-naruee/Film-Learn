47491ef006c40d024dbc675a577ca595f77c9d58
Saman-naruee <samannaruee@gmail.com>
2025-10-19 04:18:34 +0330

    start a new app: accounts. update settings. update requirements: Add django-startsubapp module to structurize apps strucutures.

--- filmlearn/settings.py
+++ filmlearn/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party apps
+    'startsubapp',
+
+    # other apps
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
